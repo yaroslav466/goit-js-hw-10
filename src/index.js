8283027dba482a04b7352fe58ba1e612ca8e63fa
yaroslav466 <ignatovjaroslaw@gmail.com>
2023-09-23 19:14:25 +0200

    commfix2

--- src/index.js
+++ src/index.js
@@ -4,46 +4,42 @@ import SlimSelect from "slim-select";
 
 const breedSelect = document.querySelector(".breed-select");
 const catInfoDiv = document.querySelector(".cat-info");
-
+const pError = document.querySelector(".error")
 const loader = document.querySelector(".loader");
 
 
 
 fetchBreeds()
   .then(breeds => {
-    catInfoDiv.classList.toggle("hidden"); 
-    breedSelect.classList.toggle("hidden")
+   
     breeds.forEach(breed => {
-    const option = document.createElement("option");
-    option.value = breed.id;
-    option.textContent = breed.name;
-    breedSelect.appendChild(option);
+      const option = document.createElement("option");
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
 
-  });
-    new SlimSelect({
-        select: '#breed-select'
     });
-})
+    new SlimSelect({
+      select: '#breed-select'
+    });
+    
+  })
   .catch(error => {
-
-
-    Notify.failure(`Oops!...Something went wrong!!!   
-                     Try reloading the page!!!`);
-    breedSelect.classList.toggle("hidden") &&
-    loader.classList.toggle("hidden"); 
-    console.error(error);
-    throw error;
+    offLoader();
+    onError();
   });
-
+offError();
+  onDisplayInfo();
+  offLoader();
+  
 
 breedSelect.addEventListener("change", () => { 
 
   const selectedBreedId = breedSelect.value;
-
-  catInfoDiv.classList.toggle("hidden"); 
-  breedSelect.classList.toggle("hidden");
-  loader.classList.toggle("hidden");
-
+ 
+ offDisplayInfo();
+  onLoader();
+  breedSelect.style.display = "flex";
 
   if (selectedBreedId && selectedBreedId != "") {
 
@@ -57,26 +53,45 @@ breedSelect.addEventListener("change", () => {
       <h2>${catData.breeds[0].name}</h2>
       <p>DESCRIPTION: ${catData.breeds[0].description}</p>
       <img src="${catData.url}" alt="${catData.breeds[0].name}" />
-    `;
+`;
         
     catInfoDiv.innerHTML = "";   
-    catInfoDiv.innerHTML += markup;
-       
-  breedSelect.classList.toggle("hidden");
-              
+    catInfoDiv.innerHTML += markup;  
+      offLoader();
+      onDisplayInfo();      
       })
       .catch(error => { 
-        Notify.failure(`Oops!...Something went wrong!!!   
-                     Try reloading the page!!!`);
-        breedSelect.classList.toggle("hidden");
-    catInfoDiv.classList.toggle("hidden");
-        console.error(error);
-        throw error;
+       offLoader();
+      onError();
       })
-       .finally(() => {
-             
-          loader.classList.toggle("hidden");
-          catInfoDiv.classList.toggle("hidden");   
-        });
+    
    }
 })
+
+
+
+
+function onError() {
+    Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
+        position: 'center-center',
+        timeout: 4000,
+        width: '300px',
+        fontSize: '24px'
+    });
+};
+
+function onLoader() {
+  loader.style.display = 'block';
+}
+function offLoader() {
+  loader.style.display = 'none';
+}
+function onDisplayInfo() {
+  catInfoDiv.style.display = 'block';
+}
+function offDisplayInfo() {
+  catInfoDiv.style.display = 'none';
+}
+function offError() {
+  pError.style.display = 'none';
+}
